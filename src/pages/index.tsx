6212c21a2603fca208ae7730e2076346584b0d3a
jorge niglia <jorge.niglia@gmail.com>
2023-05-13 18:10:21 -0300

    hasta tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <main className={`bg-slate-300 ${inter.className} h-screen  `}>
       <h1>Hello</h1>
     </main>
   );

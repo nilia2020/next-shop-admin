6a23b9022648c2a61b8144a8fe9d9831e657bbea
jorge niglia <jorge.niglia@gmail.com>
2023-05-13 23:04:17 -0300

    UI componente login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,9 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Dashboard', href: '/' },
+  { name: 'Product', href: '/' },
+  { name: 'Sales', href: '/' },
 ];
 
 export default function Home() {
@@ -38,7 +37,7 @@ export default function Home() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -66,7 +65,7 @@ export default function Home() {
                   ))}
                 </div>
                 <div className="py-6">
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                  <a href="/login" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Log in
                   </a>
                 </div>
